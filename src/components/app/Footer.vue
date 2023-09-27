cb032f9809e1a14235e4c27c9fb6d41becfd109f
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-09-27 08:34:32 +0300

    foot

--- src/components/app/Footer.vue
+++ src/components/app/Footer.vue
@@ -1,16 +1,16 @@
 <script>
 export default {
-	data() {
-		return {
-			year: (new Date()).getFullYear()
-		}
-	}
-}
+  data() {
+    return {
+      year: (new Date()).getFullYear()
+    };
+  }
+};
 </script>
 <template>
-	<!-- BEGIN #footer -->
-	<div id="footer" class="app-footer">
-		&copy; {{ year }} SmartHoldem Platform All Right Reserved
-	</div>
-	<!-- END #footer -->
+  <!-- BEGIN #footer -->
+  <div id="footer" class="app-footer">
+    &copy; {{ year }} SmartHoldem
+  </div>
+  <!-- END #footer -->
 </template>
