2fdccf49a79358a5039409ec0fca9d67f7db0c08
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-09-30 04:21:29 +0300

    warranty

---
+++ src/views/wallet/LegalWarranties.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h1>Disclaimer of Warranties</h1>
+      </div>
+      <p class="ml-3 small color-brown">20 November 2017</p>
+      <div>
+        <div class="card-body">
+          <p>You agree that your use or inability to use SmartHoldem cryptocoins is carried out solely at your own risk
+            and</p>
+          <p>you don't put any responsibility on SmartHoldem Team.</p>
+          <p>You agree that your use or inability to use SmartHoldem tokens is carried out solely at your own risk
+            and</p>
+          <p>you don't put any responsibility on SmartHoldem Team.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LegalWarranties"
+}
+</script>
+
+<style scoped>
+
+</style>
