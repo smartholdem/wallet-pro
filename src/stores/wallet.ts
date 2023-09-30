e865929d90ffc4fcab90eedd6c5bf5a080d8c6f6
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-09-30 09:07:02 +0300

    saveNewAddress

--- src/stores/wallet.ts
+++ src/stores/wallet.ts
@@ -1,12 +1,24 @@
 import { defineStore } from "pinia";
+import {generateMnemonic} from "bip39";
+import {Identities} from "@smartholdem/crypto";
 
 export const useStoreWallet = defineStore("WalletStorage", {
   state: () => ({
     accounts: {}
   }),
   actions: {
-    walletAdd(options: object) {
-
+    async getNewAddress() {
+      const mnemonic = generateMnemonic();
+      return {
+        secret: mnemonic,
+        address: Identities.Address.fromPassphrase(mnemonic, 63)
+      }
+    },
+    async saveNewAddress(options: object) {
+      this.accounts = {
+        ...this.accounts,
+        ...options,
+      };
     }
   },
   persist: true,
