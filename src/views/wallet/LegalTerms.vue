1549afa157621162de3fb5624aa120865a32a0d8
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-09-30 04:21:15 +0300

    terms

---
+++ src/views/wallet/LegalTerms.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h1>Terms and Conditions</h1>
+      </div>
+      <p class="ml-3 small color-brown">20 November 2017</p>
+      <div>
+        <div class="card-body">
+          <p>
+            Technologies related to blockchain are subject to supervision and control by various regulatory bodies
+            around the world.
+          </p>
+          <p>SmartHoldem tokens may fall under one or more requests or actions on their part, including but not limited
+            to imposing restrictions on the use or possession of digital tokens such as SmartHoldem that can slow or
+            limit the functionality of SmartHoldem tokens in the future.</p>
+          <p>
+            SmartHoldem tokens are not an investment.
+          </p>
+          <p>
+            SmartHoldem tokens are not a “surrogate currency”.
+          </p>
+          <p>
+            SmartHoldem tokens are not official or legally binding investment. Due to unforeseen circumstances, the
+            objectives described in this document may be amended. Despite the fact that we intend to reach all the
+            points described in this document, all persons and parties involved in the purchase of SmartHoldem tokens
+            do this at their own risk.
+          </p>
+          <p>
+            <span class="font-weight-bold">The risk of using new technologies.</span><br>
+            Cryptographic tokens, such as SmartHoldem, are a new and experimental technology. In addition to the
+            risks mentioned in this document, there are additional risks that the SmartHoldem team can not foresee.
+            These risks can materialize in other forms of risk than those specified here.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "LegalTerms"
+}
+</script>
+
+<style scoped>
+
+</style>
