940b7e0a417e7f0399ee40a9015b311f459f4992
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-01 06:00:23 +0300

    addressDecrypt

--- src/stores/wallet.ts
+++ src/stores/wallet.ts
@@ -1,25 +1,32 @@
 import { defineStore } from "pinia";
 import { generateMnemonic, validateMnemonic } from "bip39";
 import { Identities } from "@smartholdem/crypto";
-import {Connection} from "@smartholdem/client";
+import { Connection } from "@smartholdem/client";
+import CryptoJS from 'crypto-js'
 
 export const useStoreWallet = defineStore("WalletStorage", {
   state: () => ({
-    accounts: {}
+    accounts: {},
   }),
   actions: {
-    async getNewAddress() {
+    async addressNew() {
       const mnemonic = generateMnemonic();
       return {
         secret: mnemonic,
         address: Identities.Address.fromPassphrase(mnemonic, 63)
       };
     },
-    async saveNewAddress(options: object) {
+    async addressSave(payload: object) {
+      console.log(payload)
       this.accounts = {
         ...this.accounts,
-        ...options
+        ...payload,
       };
+    },
+    async addressDecrypt(payload: object) {
+      const hash = CryptoJS.SHA384(payload.pin).toString();
+      const accountBytes = CryptoJS.AES.decrypt(payload.secret.toString(), payload.pin + hash)
+      return accountBytes.toString(CryptoJS.enc.Utf8)
     },
     addressFromPassword(secret: string) {
       const isBip39 = validateMnemonic(secret);
@@ -27,8 +34,8 @@ export const useStoreWallet = defineStore("WalletStorage", {
       return {
         isBip39: isBip39,
         address: address,
-      }
+      };
     }
   },
-  persist: true,
+  persist: true
 });
