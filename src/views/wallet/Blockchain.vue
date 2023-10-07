8c3427afa89cc4da6c78dc4c2e8e60bb56186f3f
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-07 06:40:46 +0300

    blockchain page

---
+++ src/views/wallet/Blockchain.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-xl-12">
+      <ul class="breadcrumb">
+        <li class="breadcrumb-item"><router-link to="/">WALLET</router-link></li>
+        <li class="breadcrumb-item active text-uppercase">Blockchain</li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlockchainPage"
+};
+</script>
+
+<style scoped>
+
+</style>
