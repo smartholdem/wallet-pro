b5c951b7a22bf771f5a51d4bc6d56c6946b164ae
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-07 07:36:45 +0300

    Blockchain fees

---
+++ src/components/wallet/ComponentTransactions.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="col-xl-12 mb-3" >
+    <card v-if="transactions" style="overflow: hidden;">
+      <card-header class="card-header fw-bold small text-uppercase">Transactions</card-header>
+      <card-body style="overflow-x: auto;">
+        <table class="table table-hover" >
+          <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Time</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Sender</th>
+            <th scope="col">Recipient</th>
+            <th scope="col">Fee</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in transactions.data" :key="item.id" :class="item.confirmations < 8 ? 'table-dark' : ''">
+            <td :title="item.id">
+                <span :class="item.recipient === this.$route.params.address ? 'text-success' : ''">
+                {{ (item.id).slice(0, 5) }} .. {{ (item.id).slice(-5) }}
+                </span>
+            </td>
+            <td>
+                <span :class="item.recipient === this.$route.params.address ? 'text-success' : ''">
+                {{ tmFormat(item.timestamp.unix, "DD/MM/YY") }}
+                <span class="small">{{ format_time(item.timestamp.unix * 1000) }}</span>
+                </span>
+            </td>
+            <td>
+                <span :class="item.recipient === this.$route.params.address ? 'text-success' : ''">
+                {{ (item.amount / 1e8).toFixed(8) }}
+                </span>
+            </td>
+            <td>
+                <span :class="item.recipient === this.$route.params.address ? 'text-success' : ''">
+                {{ (item.sender).slice(0, 5) }} .. {{ (item.sender).slice(-5) }}
+                </span>
+            </td>
+            <td><span
+              :class="item.recipient === this.$route.params.address ? 'text-success' : ''">{{ (item.recipient).slice(0, 5)
+              }} .. {{ (item.recipient).slice(-5) }}</span>
+            </td>
+            <td><span
+              :class="item.recipient === this.$route.params.address ? 'text-success' : ''">{{ (item.fee / 1e8).toFixed(3)
+              }}</span>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <div>
+          <!--{{ transactions.data }}-->
+
+        </div>
+      </card-body>
+    </card>
+    <card v-if="!transactions">
+      <card-header class="card-header fw-bold small">Transactions</card-header>
+      <card-body>
+        <div>No transactions</div>
+      </card-body>
+    </card>
+  </div>
+</template>
+
+<script>
+import { storeToRefs } from "pinia";
+import { useAppOptionStore } from "@/stores/app-option";
+const appOption = useAppOptionStore();
+
+import { useStoreWallet } from "@/stores/wallet";
+const storeWallet = useStoreWallet();
+
+export default {
+  name: "ComponentTransactions",
+  props: {
+    address: String,
+  },
+  data() {
+    return {
+      timerTx: null,
+    }
+  },
+  async beforeCreate() {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    var self = this;
+    setTimeout(async () => {
+      await this.accountUpdate();
+    }, 220);
+  },
+  computed: {
+    page() {
+      return appOption.currentPage;
+    },
+    transactions() {
+      return storeWallet.transactions[this.address];
+    }
+  },
+  async mounted() {
+    if (this.currentAddress) {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      var self = this;
+      this.timerTx = setTimeout(async function tick() {
+        if (self.page === "/address/" + self.$route.params.address) {
+          await self.accountUpdate();
+          self.timerTx = setTimeout(tick, 21000); // (*)
+        } else {
+          clearTimeout(self.timerTx);
+          console.log("stop timer transactions");
+        }
+      }, 20000);
+    }
+  },
+  methods: {
+    async accountUpdate() {
+      await storeWallet.getTransactions(this.address);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/wallet/Blockchain.vue
+++ src/views/wallet/Blockchain.vue
@@ -2,17 +2,337 @@
   <div class="row justify-content-center">
     <div class="col-xl-12">
       <ul class="breadcrumb">
-        <li class="breadcrumb-item"><router-link to="/">WALLET</router-link></li>
+        <li class="breadcrumb-item">
+          <router-link to="/">WALLET</router-link>
+        </li>
         <li class="breadcrumb-item active text-uppercase">Blockchain</li>
       </ul>
+    </div>
+
+    <div class="col-xl-5 mb-3">
+      <card class="h-100" v-if="blockchain">
+        <card-header class="card-header fw-bold small text-uppercase">Static Fees</card-header>
+        <card-body>
+          <table class="table table-striped">
+            <thead>
+            <tr>
+              <th>Operation</th>
+              <th>Cost STH</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>
+                Transfer
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.transfer / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Second Signature
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.secondSignature / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Delegate Registration
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.delegateRegistration / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Vote
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.vote / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Multi Signature
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.multiSignature / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                IPFS
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.ipfs / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Multi Payment
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.multiPayment / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Delegate Resignation
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.delegateResignation / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                HTLC Lock
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.htlcLock / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                HTLC Claim
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.htlcClaim / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                HTLC Refund
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.htlcRefund / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            </tbody>
+          </table>
+
+        </card-body>
+
+
+      </card>
+    </div>
+    <!-- dynamic fees -->
+    <!--
+    <div class="col-xl-3 mb-3">
+      <card class="h-100" v-if="blockchain">
+        <card-header class="card-header fw-bold small text-uppercase">Dynamic Fees</card-header>
+        <card-body>
+          <table class="table table-striped">
+            <thead>
+            <tr>
+              <th>Operation</th>
+              <th>Cost STH</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>
+                minFeePool
+              </td>
+              <td>
+                {{ (blockchain.transactionPool.dynamicFees.minFeePool / 1e8).toFixed(8) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Second Signature
+              </td>
+              <td>
+                {{ (blockchain.transactionPool.dynamicFees.minFeeBroadcast / 1e8).toFixed(8) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Delegate Registration
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.delegateRegistration / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Vote
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.vote / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Multi Signature
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.multiSignature / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                IPFS
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.ipfs / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Multi Payment
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.multiPayment / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Delegate Resignation
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.delegateResignation / 1e8).toFixed(0) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                HTLC Lock
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.htlcLock / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                HTLC Claim
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.htlcClaim / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                HTLC Refund
+              </td>
+              <td>
+                {{ (blockchain.constants.fees.staticFees.htlcRefund / 1e8).toFixed(2) }}
+              </td>
+            </tr>
+            </tbody>
+          </table>
+
+        </card-body>
+
+
+      </card>
+    </div>
+    -->
+
+    <div class="col-xl-7 mb-3">
+      <card class="h-100" v-if="blockchain">
+        <card-header class="card-header fw-bold small text-uppercase">Blockchain</card-header>
+        <card-body>
+          <table class="table table-striped">
+            <tbody>
+            <tr>
+              <td>
+                Net Hash
+              </td>
+              <td>
+                {{ blockchain.nethash }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Token
+              </td>
+              <td>
+                {{ blockchain.token }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Version
+              </td>
+              <td>
+                {{ blockchain.version }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Slip44
+              </td>
+              <td>
+                {{ blockchain.slip44 }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                WIF
+              </td>
+              <td>
+                {{ blockchain.wif }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Height
+              </td>
+              <td>
+                {{ blockchain.constants.height }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Active Delegates
+              </td>
+              <td>
+                {{ blockchain.constants.activeDelegates }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Block Time
+              </td>
+              <td>
+                {{ blockchain.constants.blocktime }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Epoch
+              </td>
+              <td>
+                {{ blockchain.constants.epoch }}
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </card-body>
+      </card>
     </div>
 
   </div>
 </template>
 
 <script>
+import { storeToRefs } from "pinia";
+import { useStoreWallet } from "@/stores/wallet";
+
+const storeWallet = useStoreWallet();
+const { nodeConfig } = storeToRefs(storeWallet);
+
 export default {
-  name: "BlockchainPage"
+  name: "BlockchainPage",
+  async created() {
+    await storeWallet.getNodeConfig();
+  },
+  computed: {
+    blockchain() {
+      return storeWallet.nodeConfig;
+    }
+  }
 };
 </script>
 
