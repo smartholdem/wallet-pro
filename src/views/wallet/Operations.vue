318d1a32faa92b22eb1668f9d86c187b2140982e
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-17 04:15:08 +0300

    transfer modal window

--- src/views/wallet/Operations.vue
+++ src/views/wallet/Operations.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="h-100">
 
+
     <card class="h-100">
       <card-header class="card-header fw-bold small">
         OPERATIONS
@@ -17,9 +18,18 @@
         </div>
 
         <div v-if="currentAddress" class="btn-group mb-3">
+
+          <!--
           <button @click="sendTabPrepare" type="button" class="btn btn-outline-theme">
             SEND
           </button>
+          -->
+
+          <button @click="sendTabPrepare" data-bs-toggle="modal" data-bs-target="#modalTransfer" type="button" class="btn btn-outline-theme">
+            SEND
+          </button>
+
+
           <button data-bs-toggle="modal" data-bs-target="#modalQr" type="button" class="btn btn-outline-theme">
             <i class="fas fa-lg fa-fw fa-qrcode" aria-hidden="true"></i>
           </button>
@@ -40,109 +50,123 @@
           </button>
         </div>
 
-
-        <div v-if="operation === 1" class="w-100">
-          <!-- send sth prepare tx -->
-          <div v-if="txSendStep === 0">
-            <div class="row">
-              <div class="col-md-10">
-                <div class="form-group mb-3">
-                  <label class="form-label" for="sendRecipient">Recipient <i class="fa fa-address-book hover-info"></i></label>
-                  <input v-model="forSend.recipientId" @input="validateAddress" type="text" class="form-control form-control-sm" :class="forSend.addressIsValid && forSend.recipientId !== this.address ? 'is-valid' : 'is-invalid'" id="sendRecipient"
-                         placeholder="Enter address">
-                </div>
-              </div>
-              <div class="col-md-2" v-show="!isMobile">
-                <div class="form-group mb-3">
-                  <label class="form-label" for="sendFee">Fee</label>
-                  <input readonly :value="networksTransfer[selectedNetwork].fee" type="text" class="form-control form-control-sm" id="sendFee"
-                  >
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-3">
-                <div class="form-group mb-3">
-                  <label @click="forSend.amount = balanceDecimal - networksTransfer[selectedNetwork].fee" class="form-label" for="sendAmount">Amount <span class="badge text-info">[max]</span></label>
-                  <input :placeholder="'min ' + networksTransfer[selectedNetwork].minAmount" v-model="forSend.amount" type="text" class="form-control form-control-sm" :class="forSend.amount > 0.00000001 ? 'is-valid' : 'is-invalid'" id="sendAmount">
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group mb-3">
-                  <label class="form-label" for="sendMemo">Memo</label>
-                  <input :readonly="selectedNetwork !== 'mainnet'" v-model="forSend.memo" type="text" class="form-control form-control-sm" id="sendMemo"
-                         placeholder="Public Description max 250"
-                  >
-                </div>
-              </div>
-              <div class="col-md-3">
-                <div class="form-group mb-3" >
-                  <label class="form-label px-4" :class="'ico-' + selectedNetwork" for="sendNetwork" >Network</label>
-                  <select v-model="selectedNetwork" @change="validateAddress" class="form-select form-select-sm" id="sendNetwork">
-                    <option selected value="mainnet">MainNet</option>
-                    <option value="bsc">BSC</option>
-                    <option value="heco">HECO</option>
-                    <!--<option disabled value="eth">Ethereum</option>-->
-                  </select>
-
-                </div>
-              </div>
-
-            </div>
-            <div>
-              <button :disabled="((forSend.amount * 1 + networksTransfer[selectedNetwork].fee) > balanceDecimal) || !forSend.addressIsValid || forSend.amount < 0.001 || !forSend.recipientId || forSend.recipientId === this.address"
-                      @click="txSend"
-                      type="button"
-                      class="btn btn-sm"
-                      :class="(((forSend.amount * 1 + networksTransfer[selectedNetwork].fee) > balanceDecimal) || !forSend.addressIsValid || forSend.amount < 0.001 || !forSend.recipientId || forSend.recipientId === this.address) ? 'btn-secondary' : 'btn-success'">
-                SEND STH
-              </button>
-            </div>
-          </div>
-
-          <!-- tx result -->
-          <div v-if="txSendStep === 1" class="overflow-hidden overflow-x-auto">
-            <div class="mb-3">
-              <table class="table">
-                <tbody>
-                <tr>
-                  <td v-if="txErr === 0">Success txId</td>
-                  <td v-if="txErr > 0" class="text-danger">Error txId</td>
-                  <td><span class="text-primary">{{txResult.tx.id}}</span></td>
-                </tr>
-                <tr>
-                  <td>Amount</td>
-                  <td>{{(txResult.tx.amount / 1e8).toFixed(8)}} STH</td>
-                </tr>
-                <tr>
-                  <td>Fee</td>
-                  <td>{{(txResult.tx.fee / 1e8).toFixed(8) }} STH</td>
-                </tr>
-                <tr>
-                  <td>Recipient</td>
-                  <td>{{txResult.tx.recipientId}}</td>
-                </tr>
-                <tr v-if="txResult.tx.vendorField">
-                  <td>Memo</td>
-                  <td>{{txResult.tx.vendorField}}</td>
-                </tr>
-                <tr>
-                  <td>Network&nbsp;<i class="px-3 py-1" :class="'ico-' + txResult.network"></i></td>
-                  <td><span class="text-uppercase text-info">{{txResult.network}}</span></td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
-            <p>
-              <button @click="sendTabPrepare" type="button" class="btn btn-sm" :class="txErr > 0 ? 'btn-danger' : 'btn-success'">CONTINUE</button>&nbsp;<span v-show="waitConfirmTx">Please wait confirmation..</span>
-            </p>
-          </div>
-        </div>
       </card-body>
     </card>
 
+
+    <!-- modal transfer -->
+    <div class="modal fade modal-transfer" id="modalTransfer">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Transfer</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+          </div>
+          <div class="modal-body">
+            <div v-if="operation === 1" class="w-100">
+
+              <div v-if="txSendStep === 0">
+                <div class="row">
+                  <div class="col-md-10">
+                    <div class="form-group mb-3">
+                      <label class="form-label" for="sendRecipient">Recipient <i class="fa fa-address-book hover-info"></i></label>
+                      <input v-model="forSend.recipientId" @input="validateAddress" type="text" class="form-control form-control-sm" :class="forSend.addressIsValid && forSend.recipientId !== this.address ? 'is-valid' : 'is-invalid'" id="sendRecipient"
+                             placeholder="Enter address">
+                    </div>
+                  </div>
+                  <div class="col-md-2" v-show="!isMobile">
+                    <div class="form-group mb-3">
+                      <label class="form-label" for="sendFee">Fee</label>
+                      <input readonly :value="networksTransfer[selectedNetwork].fee" type="text" class="form-control form-control-sm" id="sendFee"
+                      >
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="form-group mb-3">
+                      <label @click="forSend.amount = balanceDecimal - networksTransfer[selectedNetwork].fee" class="form-label" for="sendAmount">Amount <span class="badge text-info">[max]</span></label>
+                      <input :placeholder="'min ' + networksTransfer[selectedNetwork].minAmount" v-model="forSend.amount" type="text" class="form-control form-control-sm" :class="forSend.amount > 0.00000001 ? 'is-valid' : 'is-invalid'" id="sendAmount">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group mb-3">
+                      <label class="form-label" for="sendMemo">Memo</label>
+                      <input :readonly="selectedNetwork !== 'mainnet'" v-model="forSend.memo" type="text" class="form-control form-control-sm" id="sendMemo"
+                             placeholder="Public Description max 250"
+                      >
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="form-group mb-3" >
+                      <label class="form-label px-4" :class="'ico-' + selectedNetwork" for="sendNetwork" >Network</label>
+                      <select v-model="selectedNetwork" @change="validateAddress" class="form-select form-select-sm" id="sendNetwork">
+                        <option selected value="mainnet">MainNet</option>
+                        <option value="bsc">BSC</option>
+                        <option value="heco">HECO</option>
+                        <option disabled value="eth">Ethereum</option>
+                      </select>
+
+                    </div>
+                  </div>
+
+                </div>
+                <div>
+                  <button :disabled="((forSend.amount * 1 + networksTransfer[selectedNetwork].fee) > balanceDecimal) || !forSend.addressIsValid || forSend.amount < 0.001 || !forSend.recipientId || forSend.recipientId === this.address"
+                          @click="txSend"
+                          type="button"
+                          class="btn btn-sm"
+                          :class="(((forSend.amount * 1 + networksTransfer[selectedNetwork].fee) > balanceDecimal) || !forSend.addressIsValid || forSend.amount < 0.001 || !forSend.recipientId || forSend.recipientId === this.address) ? 'btn-secondary' : 'btn-success'">
+                    SEND STH
+                  </button>
+                </div>
+              </div>
+
+              <div v-if="txSendStep === 1" class="overflow-hidden overflow-x-auto">
+                <div class="mb-3">
+                  <table class="table">
+                    <tbody>
+                    <tr>
+                      <td v-if="txErr === 0">Success txId</td>
+                      <td v-if="txErr > 0" class="text-danger">Error txId</td>
+                      <td><span class="text-primary">{{txResult.tx.id}}</span></td>
+                    </tr>
+                    <tr>
+                      <td>Amount</td>
+                      <td>{{(txResult.tx.amount / 1e8).toFixed(8)}} STH</td>
+                    </tr>
+                    <tr>
+                      <td>Fee</td>
+                      <td>{{(txResult.tx.fee / 1e8).toFixed(8) }} STH</td>
+                    </tr>
+                    <tr>
+                      <td>Recipient</td>
+                      <td>{{txResult.tx.recipientId}}</td>
+                    </tr>
+                    <tr v-if="txResult.tx.vendorField">
+                      <td>Memo</td>
+                      <td>{{txResult.tx.vendorField}}</td>
+                    </tr>
+                    <tr>
+                      <td>Network&nbsp;<i class="px-3 py-1" :class="'ico-' + txResult.network"></i></td>
+                      <td><span class="text-uppercase text-info">{{txResult.network}}</span></td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <p>
+                  <button @click="sendTabPrepare" type="button" class="btn btn-sm" :class="txErr > 0 ? 'btn-danger' : 'btn-success'">CONTINUE</button>&nbsp;<span v-show="waitConfirmTx">Please wait confirmation..</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
     <!-- modal decrypt -->
-    <div class="modal fade fade" id="modalDecryptAddress">
+    <div class="modal fade" id="modalDecryptAddress">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -331,5 +355,9 @@ export default {
 </script>
 
 <style scoped>
+
+.modal-transfer {
+  --bs-modal-width: 700px;
+}
 
 </style>
