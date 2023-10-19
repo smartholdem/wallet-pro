5a756979536eb8fa24ee14eadb32f72fa41018db
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-19 04:39:09 +0300

    modal vote

---
+++ src/components/wallet/ModalVote.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- modal vote -->
+  <div class="modal fade modal-transfer" id="modalVote">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Delegate Vote</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+          <div class="w-100">
+            Vote content
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalVote"
+};
+</script>
+
+<style scoped>
+
+</style>
