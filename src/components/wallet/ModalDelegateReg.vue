8efb5debcc29179f1c0757d2cf63318093f34551
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-11-20 11:10:36 +0300

    modalDelegateReg

---
+++ src/components/wallet/ModalDelegateReg.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="modal fade modal-transfer" id="modalDelegateReg">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Vote for Delegate</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div v-if="currentAddress" class="modal-body">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStoreWallet } from "@/stores/wallet.ts";
+const storeWallet = useStoreWallet();
+
+export default {
+  name: "ModalDelegateReg",
+  props: {
+    address: String,
+  },
+  computed: {
+    balanceDecimal() {
+      return this.currentAddress.balance / 10 ** 8;
+    },
+    currentAddress() {
+      return storeWallet.attributes[this.address];
+    },
+  },
+
+};
+</script>
+
+<style scoped>
+
+</style>
