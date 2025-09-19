a2a71fc5ab96cf7327d3b58476661b5240c5d8d8
TechnoL0g <technologies.bro@gmail.com>
2025-09-19 03:18:02 +0300

    Refactor app sidebar menu and router configuration for improved readability

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,56 +1,60 @@
 import {
-  createRouter,
-  createWebHistory,
-  createWebHashHistory,
+    createRouter,
+    createWebHistory,
+    createWebHashHistory,
 } from "vue-router";
 
 const isElectron =
-  typeof navigator !== "undefined" &&
-  navigator.userAgent.toLowerCase().includes(" electron/");
+    typeof navigator !== "undefined" &&
+    navigator.userAgent.toLowerCase().includes(" electron/");
 const history = isElectron
-  ? createWebHashHistory(import.meta.env.BASE_URL)
-  : createWebHistory(import.meta.env.BASE_URL);
+    ? createWebHashHistory(import.meta.env.BASE_URL)
+    : createWebHistory(import.meta.env.BASE_URL);
 
 const router = createRouter({
-  history,
-  routes: [
-    { path: "/", component: () => import("../views/wallet/Wallet.vue") },
-    {
-      path: "/address/:address",
-      component: () => import("../views/wallet/Address.vue"),
-    },
-    {
-      path: "/delegates",
-      component: () => import("../views/wallet/Delegates.vue"),
-    },
-    {
-      path: "/limitations",
-      component: () => import("../views/wallet/LegalLimitations.vue"),
-    },
-    {
-      path: "/terms",
-      component: () => import("../views/wallet/LegalTerms.vue"),
-    },
-    {
-      path: "/warranties",
-      component: () => import("../views/wallet/LegalWarranties.vue"),
-    },
-    {
-      path: "/blockchain",
-      component: () => import("../views/wallet/Blockchain.vue"),
-    },
-    { path: "/page/error", component: () => import("../views/PageError.vue") },
-    { path: "/login", component: () => import("../views/PageLogin.vue") },
-    { path: "/register", component: () => import("../views/PageRegister.vue") },
-    {
-      path: "/address-book",
-      component: () => import("../views/wallet/AddressBook.vue"),
-    },
-    {
-      path: "/:pathMatch(.*)*",
-      component: () => import("../views/PageError.vue"),
-    },
-  ],
+    history,
+    routes: [
+        {path: "/", component: () => import("../views/wallet/Wallet.vue")},
+        {
+            path: "/address/:address",
+            component: () => import("../views/wallet/Address.vue"),
+        },
+        {
+            path: "/delegates",
+            component: () => import("../views/wallet/Delegates.vue"),
+        },
+        {
+            path: "/limitations",
+            component: () => import("../views/wallet/LegalLimitations.vue"),
+        },
+        {
+            path: "/terms",
+            component: () => import("../views/wallet/LegalTerms.vue"),
+        },
+        {
+            path: "/warranties",
+            component: () => import("../views/wallet/LegalWarranties.vue"),
+        },
+        {
+            path: "/blockchain",
+            component: () => import("../views/wallet/Blockchain.vue"),
+        },
+        {
+            path: "/settings",
+            component: () => import("../views/Settings.vue"),
+        },
+        {path: "/page/error", component: () => import("../views/PageError.vue")},
+        {path: "/login", component: () => import("../views/PageLogin.vue")},
+        {path: "/register", component: () => import("../views/PageRegister.vue")},
+        {
+            path: "/address-book",
+            component: () => import("../views/wallet/AddressBook.vue"),
+        },
+        {
+            path: "/:pathMatch(.*)*",
+            component: () => import("../views/PageError.vue"),
+        },
+    ],
 });
 
 export default router;
--- src/stores/app-sidebar-menu.ts
+++ src/stores/app-sidebar-menu.ts
@@ -1,50 +1,51 @@
-import { defineStore } from "pinia";
+import {defineStore} from "pinia";
 
 export const useAppSidebarMenuStore = defineStore({
-  id: "appSidebarMenu",
-  state: () => {
-    return [
-      {
-        text: "navigation",
-        is_header: true,
-      },
-      {
-        url: "/",
-        icon: "fas fa-user",
-        text: "wallet",
-      },
-      {
-        url: "/delegates",
-        icon: "fas fa-list-ol",
-        text: "delegates",
-      },
-      {
-        url: "/blockchain",
-        icon: "fas fa-braille",
-        text: "blockchain",
-      },
-      {
-        url: "/address-book",
-        icon: "fa fa-address-book",
-        text: "address_book",
-      },
-      /**
-      {
-        "url": "/settings",
-        "icon": "bi bi-gear",
-        "text": "settings"
-      },
-      {
-        "url": "/dashboard",
-        "icon": "bi bi-cpu",
-        "text": "dashboard"
-      },
-      {
-        "url": "/analytics",
-        "icon": "bi bi-bar-chart",
-        "text": "analytics"
-      },
-      **/
-    ];
-  },
+    id: "appSidebarMenu",
+    state: () => {
+        return [
+            {
+                text: "navigation",
+                is_header: true,
+            },
+            {
+                url: "/",
+                icon: "fas fa-user",
+                text: "wallet",
+            },
+            {
+                url: "/delegates",
+                icon: "fas fa-list-ol",
+                text: "delegates",
+            },
+            {
+                url: "/blockchain",
+                icon: "fas fa-braille",
+                text: "blockchain",
+            },
+            {
+                url: "/address-book",
+                icon: "fa fa-address-book",
+                text: "address_book",
+            },
+            {
+                "url": "/settings",
+                "icon": "bi bi-gear",
+                "text": "settings"
+            },
+            /**
+
+             {
+             "url": "/dashboard",
+             "icon": "bi bi-cpu",
+             "text": "dashboard"
+             },
+             {
+             "url": "/analytics",
+             "icon": "bi bi-bar-chart",
+             "text": "analytics"
+             },
+             **/
+        ];
+    },
 });
