d69002e234be00eb9321e19e7451c81b82c4d330
TechnoL0g <technologies.bro@gmail.com>
2025-09-20 06:11:40 +0300

    Update slippage value and enhance price fetching logic in exchange module

--- src/stores/exchange.ts
+++ src/stores/exchange.ts
@@ -2,7 +2,8 @@ import { defineStore } from "pinia";
 import axios from "axios";
 
 // Константы для подключения
-const EXCHANGE_API_URL = "http://localhost:3302";
+//const EXCHANGE_API_URL = "https://exchange.smartholdem.io"; //;
+const EXCHANGE_API_URL = "http://localhost:3302"
 
 export const useExchangeStore = defineStore("exchange", {
   state: () => ({
@@ -10,6 +11,10 @@ export const useExchangeStore = defineStore("exchange", {
     sellGateAddress: null,
     sth_usdt_price: 0,
     error: null,
+    realPrice: 0, // Храним динамическую цену
+    calculatedBuyAmount: 0, // Рассчитанное количество STH при покупке
+    calculatedReceiveUsdtAmount: 0, // Рассчитанное количество USDT при продаже
+    calculatedUsdtAmountForBuy: 0, // Рассчитанное количество USDT для покупки определенного количества STH
   }),
   actions: {
     /**
@@ -36,10 +41,12 @@ export const useExchangeStore = defineStore("exchange", {
      * @param userId - Адрес пользователя в сети SmartHoldem
      */
     async getDepositAddress(network: string, userId: string) {
+        console.log('getDepositAddress', userId)
       try {
         const response = await axios.get(
           `${EXCHANGE_API_URL}/exchange-address/${network}/${userId}`
         );
+        console.log(response.data);
         if (response.data && response.data.address) {
           this.depositAddress = response.data.address;
           this.error = null;
@@ -69,6 +76,93 @@ export const useExchangeStore = defineStore("exchange", {
         console.error("Ошибка при получении адреса шлюза для продажи:", e);
         this.error = "exchange_error_failed_to_get_sell_address";
         this.sellGateAddress = null;
+      }
+    },
+
+    /**
+     * Получает актуальную цену из пула ликвидности с учетом проскальзывания для покупки STH.
+     * @param sthAmount - Количество STH, которое хочет купить пользователь
+     */
+    async fetchRealPriceForBuy(sthAmount: number) {
+      try {
+        // Оцениваем, сколько USDT нужно, чтобы купить sthAmount STH, используя текущую цену
+        const estimatedUsdtAmount = sthAmount * this.sth_usdt_price;
+        const url = `${EXCHANGE_API_URL}/xbts/pool/sth-usdt-real?usdt_amount=${estimatedUsdtAmount}`;
+
+        const response = await axios.get(url);
+        const data = response.data;
+
+        // Проверяем на актуальный формат ответа: {usdtAmount, estimatedSthToReceive, minSthToReceive, effectivePrice}
+        // Ранее был формат: {usdtAmount, sthToReceive, effectivePrice}
+        if (data && data.effectivePrice !== undefined && data.usdtAmount !== undefined && 
+            (data.estimatedSthToReceive !== undefined || data.sthToReceive !== undefined)) {
+          // effectivePrice - это уже готовый курс с учетом проскальзывания
+          // usdtToPay = sthAmount * effectivePrice
+          const usdtToPay = sthAmount * data.effectivePrice;
+          
+          this.calculatedUsdtAmountForBuy = usdtToPay;
+          // Обновим realPrice для отладки или других целей, если нужно
+          this.realPrice = data.effectivePrice;
+          // Также обновим calculatedBuyAmount для обратной совместимости или других целей
+          // calculatedBuyAmount - это количество STH, которое мы получим за data.usdtAmount
+          // Берем estimatedSthToReceive если есть, иначе sthToReceive
+          this.calculatedBuyAmount = data.estimatedSthToReceive !== undefined ? data.estimatedSthToReceive : data.sthToReceive;
+        } else {
+          console.error("Ошибка получения данных о реальной цене для покупки (неверный формат):", data);
+          this.calculatedUsdtAmountForBuy = 0;
+        }
+      } catch (error) {
+        console.error("Ошибка при вызове fetchRealPriceForBuy:", error);
+        this.calculatedUsdtAmountForBuy = 0;
+      }
+    },
+
+    /**
+     * Получает актуальную цену из пула ликвидности с учетом проскальзывания.
+     * @param amount - Количество STH или USDT
+     * @param isBuyingSth - true, если покупаем STH (amount это USDT), false, если продаем STH (amount это STH)
+     */
+    async fetchRealPrice(amount: number, isBuyingSth: boolean = true) {
+      try {
+        let url;
+        if (isBuyingSth) {
+          // amount это количество USDT
+          url = `${EXCHANGE_API_URL}/xbts/pool/sth-usdt-real?usdt_amount=${amount}`;
+        } else {
+          // amount это количество STH, но эндпоинт требует usdt_amount для расчета.
+          // Нужно сначала примерно оценить эквивалент в USDT, чтобы вызвать эндпоинт.
+          // Используем старую цену для оценки.
+          const estimatedUsdtAmount = amount * this.sth_usdt_price;
+          url = `${EXCHANGE_API_URL}/xbts/pool/sth-usdt-real?usdt_amount=${estimatedUsdtAmount}`;
+        }
+
+        const response = await axios.get(url);
+        const data = response.data;
+
+        if (data && data.minSthToReceive !== undefined && data.estimatedSthToReceive !== undefined) {
+          if (isBuyingSth) {
+            // При покупке STH мы получаем calculatedBuyAmount
+            this.calculatedBuyAmount = parseFloat(data.minSthToReceive);
+            // Обновим realPrice для отладки или других целей, если нужно
+            this.realPrice = data.usdt_amount / data.estimatedSthToReceive;
+          } else {
+            // При продаже STH мы получаем calculatedReceiveUsdtAmount
+            // data.estimatedSthToReceive это количество STH, которое мы бы получили за data.usdt_amount.
+            // Нам нужно пересчитать, сколько USDT мы получим за наш sellAmount.
+            // Проще всего: использовать estimatedSthToReceive для расчета идеальной цены,
+            // а затем применить проскальзывание.
+            const idealRate = data.usdt_amount / data.estimatedSthToReceive;
+            this.calculatedReceiveUsdtAmount = amount * idealRate; // amount это sellAmount
+            // Обновим realPrice для отладки или других целей, если нужно
+            this.realPrice = idealRate;
+          }
+        } else {
+          console.error("Ошибка получения данных о реальной цене:", data);
+          // В случае ошибки можно использовать старую цену или показать сообщение
+        }
+      } catch (error) {
+        console.error("Ошибка при вызове fetchRealPrice:", error);
+        // В случае ошибки можно использовать старую цену или показать сообщение
       }
     },
   },
