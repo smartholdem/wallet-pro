2bd9d3ce7fe4c31092b8348821a16c860f824a1f
TechnoL0g <technologies.bro@gmail.com>
2025-10-22 10:12:41 +0300

    feat: implement multilingual support for SmartNote information modal

--- src/components/wallet/GmInfoModal.vue
+++ src/components/wallet/GmInfoModal.vue
@@ -1,43 +1,44 @@
 <template>
   <div class="modal fade" id="gmInfoModal" tabindex="-1">
     <div class="modal-dialog modal-lg modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Информация о Smartnote</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-        </div>
-        <div class="modal-body">
-          <h5>Smartnote - Анонимный актив на блокчейне SmartHoldem.</h5>
-          <p>
-            SmartNote - это умные крипто деньги для всего цифрового мира. Это быстро, приватно и безопасно. Вы можете спокойно тратить SmartNote, зная, что другие не могут видеть ваши балансы или отслеживать вашу деятельность.
-          </p>
-          <p>
-            Smartnote – не отслеживаемая цифровая валюта нового поколения на основе криптографии, обеспечивающая защиту личных данных и полную конфиденциальность.
-          </p>
-          <p>Не отслеживаемость. Для каждой входящей транзакции все отправители равновероятны.</p>
-          <p>Несвязанность. Для двух любых исходящих транзакций невозможно доказать, что они отправлены одним и тем же лицом.</p>
-          <p>
-            Держатель SmartNote может представить другому пользователю информацию о коде для активации, проверки или отправки. Изображение выпущенных Smartnote могут распространяться публично без ограничений.
-          </p>
-          <p>
-            Вы можете выпустить, распечатать и оправить купюру по почте или вручить лично, также SmartNote можно хранить в сейфе, подарить на День Рождения или отправить изображение SmartNote по электронной почте, через мессенджер, аккаунт в социальной сети или любым другим способом.
-          </p>
-          <p>
-            Smartnote — это умная электронная наличность, предоставляющая анонимность и приватность своему владельцу.
-          </p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
-        </div>
-      </div>
+      <component :is="SmartNoteInfo" />
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "GmInfoModal"
-}
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import { computed } from "vue";
+const { locale } = useI18n();
+
+
+import NoteInfo_ru from "@/components/SmartNoteInfo/SmartNoteInfo_ru.vue";
+import NoteInfo_es from "@/components/SmartNoteInfo/SmartNoteInfo_ru.vue";
+import NoteInfo_de from "@/components/SmartNoteInfo/SmartNoteInfo_ru.vue";
+import NoteInfo_fr from "@/components/SmartNoteInfo/SmartNoteInfo_ru.vue";
+import NoteInfo_vi from "@/components/SmartNoteInfo/SmartNoteInfo_ru.vue";
+import NoteInfo_id from "@/components/SmartNoteInfo/SmartNoteInfo_ru.vue";
+import NoteInfo_en from "@/components/SmartNoteInfo/SmartNoteInfo_ru.vue";
+
+const SmartNoteInfo = computed(() => {
+  switch (locale.value) {
+    case "ru":
+      return NoteInfo_ru;
+    case "es":
+      return NoteInfo_es;
+    case "de":
+      return NoteInfo_de;
+    case "fr":
+      return NoteInfo_fr;
+    case "vi":
+      return NoteInfo_vi;
+    case "id":
+      return NoteInfo_id;
+    default:
+      return NoteInfo_en;
+  }
+});
+
 </script>
 
 <style scoped>
